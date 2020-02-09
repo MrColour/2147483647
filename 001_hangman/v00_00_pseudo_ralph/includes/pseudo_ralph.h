2dc3ca42866ae579f455d863d33fd3ae4a3b60fc
Kevin Mira <kmira@e1z2r5p16.42.us.org>
2020-02-09 09:44:16 -0800

    GIT:  001:pseudo_ralph: GIF COOKIE: pseudo_ralph

--- 001_hangman/v00_00_pseudo_ralph/includes/pseudo_ralph.h
+++ 001_hangman/v00_00_pseudo_ralph/includes/pseudo_ralph.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pseudo_ralph.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xinu <xinu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kmira <kmira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 02:53:42 by kmira             #+#    #+#             */
-/*   Updated: 2020/02/08 08:48:58 by xinu             ###   ########.fr       */
+/*   Updated: 2020/02/09 09:04:24 by kmira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # define WRONG -1
 # define RIGHT  1
 
-#define MAX_LIVES 20
+#define MAX_LIVES 7
 
 # define ANSI_RESET "\033[0m"
 # define ANSI_CLEAR "\33[2K"
--- 001_hangman/v00_00_pseudo_ralph/srcs/main.c
+++ 001_hangman/v00_00_pseudo_ralph/srcs/main.c
@@ -25,6 +25,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <time.h>
 
 char	*strlwr(const char *str)
 {
@@ -105,6 +106,7 @@ int		main(void)
 	wrong_guesses = 0;
 	str = strlwr(phrase);
 	printf("\n");
+	srand(time(0));
 	while (i < 100)
 	{
 		guess = (rand() % ('z' - 'a')) + 'a';
@@ -132,9 +134,10 @@ int		main(void)
 		printf("\n");
 		print_phrase_state(phrase_state);
 		printf("\n");
-		cur_up(4);
 		if (wrong_guesses >= MAX_LIVES)
 		{
+			usleep(1000000);
+			cur_up(4);
 			print_game_over();
 			break ;
 		}
@@ -143,10 +146,11 @@ int		main(void)
 			print_game_win();
 			break ;
 		}
-		usleep(300000);
+		cur_up(4);
+		usleep(500000);
 		i++;
 	}
-	cur_up(2);
-	printf(ANSI_CLEAR"\r");
+	// cur_up(2);
+	// printf("\r");
 	return (0);
 }
